be5f74a7872a6e7a141418121d3b3e1f01cfa837
edwin <eddiendiritu@gmail.com>
2022-12-28 08:48:58 +0300

    phone authentication

---
+++ lib/screens/home/components/received_songs.dart
@@ -0,0 +1,167 @@
+import 'package:assorted_layout_widgets/assorted_layout_widgets.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:soul_date/components/image_circle.dart';
+import 'package:text_scroll/text_scroll.dart';
+
+import '../../../constants/constants.dart';
+
+class ReceivedSongsList extends StatelessWidget {
+  const ReceivedSongsList({
+    Key? key,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: defaultMargin),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Your Vinyls",
+                    style: Theme.of(context).textTheme.headline5,
+                  ),
+                  Text(
+                    "Check out what your friends sent you",
+                    style: Theme.of(context).textTheme.caption,
+                  )
+                ],
+              ),
+              const Spacer(),
+              Text(
+                "View All",
+                style: Theme.of(context)
+                    .textTheme
+                    .headline6!
+                    .copyWith(color: Theme.of(context).primaryColor),
+              )
+            ],
+          ),
+          ListView.builder(
+            primary: false,
+            itemBuilder: (context, index) {
+              return _ReceivedSongCard(index: index);
+            },
+            itemCount: 2,
+            shrinkWrap: true,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _ReceivedSongCard extends StatelessWidget {
+  const _ReceivedSongCard({
+    Key? key,
+    required this.index,
+  }) : super(key: key);
+
+  final int index;  
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    Radius radius = const Radius.circular(20);
+    Radius zeroRadius = const Radius.circular(0);
+    return Container(
+        height: 120,
+        width: size.width,
+        padding: const EdgeInsets.symmetric(
+            horizontal: defaultMargin, vertical: defaultPadding),
+        margin: EdgeInsets.only(
+            top: defaultMargin * 2,
+            right: index.isEven ? defaultMargin : 0.0,
+            left: index.isOdd ? defaultMargin : 0.0),
+        decoration: BoxDecoration(
+            color: Colors.black, borderRadius: BorderRadius.all(radius)),
+        child: Row(
+          children: [
+            SoulCircleAvatar(
+              imageUrl: defaultGirlUrl,
+              radius: 22,
+            ),
+            SizedBox(
+              width: defaultMargin,
+            ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Align(
+                    alignment: Alignment.bottomRight,
+                    child: Padding(
+                      padding:
+                          const EdgeInsets.symmetric(horizontal: defaultMargin),
+                      child: RowSuper(children: [
+                        SoulCircleAvatar(
+                          imageUrl: secondaryAvatarUrl,
+                          radius: 12,
+                        )
+                      ]),
+                    ),
+                  ),
+                  Text(
+                    "Hope Celestine",
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodyText1!
+                        .copyWith(color: Colors.white),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.symmetric(vertical: defaultMargin / 2),
+                    child: Row(
+                      children: [
+                        SoulCircleAvatar(
+                          imageUrl: defaultArtistUrl,
+                          radius: 12,
+                        ),
+                        const SizedBox(
+                          width: defaultPadding,
+                        ),
+                        TextScroll(
+                          "Perfect. Ed Sheeran",
+                          style: Theme.of(context)
+                              .textTheme
+                              .caption!
+                              .copyWith(color: spotifyGreen),
+                        )
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: defaultPadding,
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        "Hope:",
+                        style: Theme.of(context).textTheme.caption,
+                      ),
+                      const SizedBox(
+                        width: defaultPadding,
+                      ),
+                      Expanded(
+                        child: Text(
+                          "Omg Guys you need to listen to this",
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          style: Theme.of(context).textTheme.caption,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            )
+          ],
+        ));
+  }
+}
